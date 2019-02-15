1712bbeb60d79e132e2932c8eeee10ab7b8e9535
Gregory N. Schmit <schmitgreg@gmail.com>
2019-02-15 09:59:54 -0600

    bugfix: fix self.tables access in purge method

--- django_purge/models.py
+++ django_purge/models.py
@@ -36,7 +36,7 @@ class DatabasePurger(models.Model):
         depending on the configuration.
         """
         if not self.enabled: return
-        model_list = [x.model_class() for x in self.tables]
+        model_list = [x.model_class() for x in self.tables.all()]
         d = timezone.now() - datetime.timedelta(days=self.age_in_days)
         datetime_filter = {self.datetime_field + '__lt': d}
         if self.delete_by_age:
